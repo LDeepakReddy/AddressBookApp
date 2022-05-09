db44a21859fb53cfcf8c4fb5c14d6bc1f52e9cd2
LDeepakReddy <l.deepakreddy.com>
2022-05-09 22:38:58 +0530

    ADDED: Getters and Setters for AddressBookApp to capture the User Emtries

--- Js/AddressBook_Form.js
+++ Js/AddressBook_Form.js
@@ -17,22 +17,22 @@ window.addEventListener('DOMContentLoaded', (event) => {
         }
     });
 
-    const addressElement = document.querySelector('#address');
-    const addressError = document.querySelector('#address-error');
-    addressElement.addEventListener('input', function () {
-        let address = document.querySelector('#address').value;
-        if (addressElement.value.length == 0) {
-            addressError.textContent = '';
-            return;
-        }
-        try {
-            (new Contact()).address = address;
-            addressError.textContent = '';
-            return;
-        } catch (e) {
-            addressError.textContent = e;
-        }
-    });
+    // const addressElement = document.querySelector('#address');
+    // const addressError = document.querySelector('#address-error');
+    // addressElement.addEventListener('input', function () {
+    //     let address = document.querySelector('#address').value;
+    //     if (addressElement.value.length == 0) {
+    //         addressError.textContent = '';
+    //         return;
+    //     }
+    //     try {
+    //         (new Contact()).address = address;
+    //         addressError.textContent = '';
+    //         return;
+    //     } catch (e) {
+    //         addressError.textContent = e;
+    //     }
+    // });
 
 
     const phoneElement = document.querySelector('#phone');
@@ -48,46 +48,63 @@ window.addEventListener('DOMContentLoaded', (event) => {
         }
     });
 
+   
 
+    // const emailElement = document.querySelector('#email');
+    // const emailError = document.querySelector('.email-error');
+    // emailElement.addEventListener('input', function () {
+    //     let email = document.querySelector('#email').value;
+    //     try {
+    //         (new Contact()).email = email;
+    //         emailError.textContent = "";
+    //     } catch (e) {
+    //         emailError.textContent = e;
+    //     }
 
-    const emailElement = document.querySelector('#email');
-    const emailError = document.querySelector('.email-error');
-    emailElement.addEventListener('input', function () {
-        let email = document.querySelector('#email').value;
-        try {
-            (new Contact()).email = email;
-            emailError.textContent = "";
-        } catch (e) {
-            emailError.textContent = e;
-        }
-
-    });
-    const save = (event) => {
-    
-        try {
-            setContactObject();
-        } catch (e) {
-            console.log(e);
-            return;
-        }
-    }
-
-    const setContactObject = () => {
-    
-        let names = getInputValueById('#name').split(" ");
-        contactObject._firstName = names[0];
-        contactObject._lastName = names[1];
-        contactObject._address = getInputValueById('#address');
-        contactObject._city = getInputValueById('#city');
-        contactObject._state = getInputValueById('#state');
-        contactObject._zip = getInputValueById('#zip');
-        contactObject._phone = getInputValueById('#phone');
-        contactObject._email = getInputValueById('#email');
-        alert(JSON.stringify(contactObject))
-    }
-
-    const getInputValueById = (id) => {
-        let value = document.querySelector(id).value;
-        return value;
-    }
+    // });
 })
+
+const save = (event)=>{
+//     event.preventDefault();
+//   event.stopPropagation();
+  try{
+    setContactObject();
+    createAddressBook();
+  }catch(e){
+    console.log(e);
+    return;
+  }
+}
+
+const setContactObject = () => {
+    let names = getInputValueById('#name').split(" ");
+    contactObject._firstName = names[0];
+    contactObject._lastName = names[1];
+    contactObject._address = getInputValueById('#address');
+    contactObject._city = getInputValueById('#city');
+    contactObject._state = getInputValueById('#state');
+    contactObject._zip = getInputValueById('#zip');
+    contactObject._phone = getInputValueById('#phone');
+    contactObject._email = getInputValueById('#email');
+    // alert(JSON.stringify(contactObject))
+  }
+
+  const createAddressBook = () =>{
+      let addressbook = new Contact();
+      let names = getInputValueById('#name').split(" ");
+    addressbook._firstName = names[0];
+    addressbook._lastName = names[1];
+    addressbook._address = getInputValueById('#address');
+    addressbook._city = getInputValueById('#city');
+    addressbook._state = getInputValueById('#state');
+    addressbook._zip = getInputValueById('#zip');
+    addressbook._phone = getInputValueById('#phone');
+    addressbook._email = getInputValueById('#email');
+    alert(addressbook)
+
+  }
+
+  const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+  }
