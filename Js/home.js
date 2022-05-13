99f16485ff3fa148ccbc4c37a10be7bc78a73dc3
LDeepakReddy <l.deepakreddy.com>
2022-05-14 01:15:27 +0530

    ADDED: function to populate data form local storage to homepage table

---
+++ Js/home.js
@@ -0,0 +1,45 @@
+let addressBookList;
+window.addEventListener('DOMContentLoaded', () => {
+    addressBookList = getDataFromStorage();
+    document.querySelector(".person-count").textContent = addressBookList.length;
+    createInnerHTML();
+    localStorage.removeItem("edit-person");
+})
+
+/** UC4 section 2  populate data from local storage in home page table */
+const getDataFromStorage = () => {
+    return localStorage.getItem('AddressBookList') ?
+        JSON.parse(localStorage.getItem('AddressBookList')) : [];
+}
+
+const createInnerHTML = () => {
+    const headerHtml = `
+    <tr>
+        <th>Fullname</th>
+        <th>Address</th>
+        <th>City</th>
+        <th>State</th>
+        <th>Zip Code</th>
+        <th>Phone Number</th>
+        <th>Actions</th>
+    </tr>`;
+    if (addressBookList.length == 0) return;
+    let innerHtml = `${headerHtml}`;
+    for (const contactData of addressBookList) {
+        innerHtml = `${innerHtml}
+        <tr>
+            <td>${contactData._name}</td>
+            <td>${contactData._address}</td>
+            <td>${contactData._city}</td>
+            <td>${contactData._state}</td>
+            <td>${contactData._zipcode}</td>
+            <td>${contactData._phone}</td>
+            <td>
+            <img id="${contactData._id}" alt="edit" src="../assets/icons/create-black-18dp.svg" onClick=update(this)>
+            <img id="${contactData._id}" alt="delete" src="../assets/icons/delete-black-18dp.svg" onClick=remove(this)>
+            </td>
+        </tr>
+        `;
+    }
+    document.querySelector('#display').innerHTML = innerHtml;
+}
